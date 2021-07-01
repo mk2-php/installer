3525b7991ce93de1bbf72fd535372ebe7d866a83
nakatsuji masato <nakatsuji@teastalk.jp>
2021-07-01 10:43:22 +0900

    7/1 Add Readme.md

--- src/app/Controller/AppController.php
+++ src/app/Controller/AppController.php
@@ -1,14 +1,23 @@
 <?php
 
 /**
- * PHP-Framework - Mark2
+ * ===================================================
+ * 
+ * PHP Framework "Mk2"
+ *
  * AppController
  * 
- * @copylight Satoru-Nakajima
+ * URL : https://www.mk2-php.com/
+ * 
+ * Copylight : Nakajima-Satoru 2021.
+ *           : Sakaguchiya Co. Ltd. (https://www.teastalk.jp/)
+ * 
+ * ===================================================
  */
 
 namespace App\Controller;
 
+// The inheritance based on "Controller" use.
 use Mk2\Libraries\Controller;
 
 class AppController extends Controller{
@@ -17,15 +26,11 @@ class AppController extends Controller{
 	public $autoRender=true;
 	
 	public function handleBefore(){
-		/**
-		 * handle before area
-		 */
+		// handle before area
 	}
 	
 	public function handleAfter($input){
-		/**
-		 * handle after area
-		 */
+		// handle after area
 	}
 
 }
